bb87e7b868a36acc7a4795f5efbce887a650bbdb
Bagas Pratama <bagassss.com@gmail.com>
2025-11-03 20:28:09 +0700

    integrasi checkout dengan detail produk

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tubes_sparehub/pages/halaman_checkout.dart';
 import 'package:tubes_sparehub/pages/halaman_profil/halaman_edit_profil.dart';
 import 'package:tubes_sparehub/pages/homepage.dart';
-import 'package:tubes_sparehub/pages/halaman_checkout.dart';
 import 'package:tubes_sparehub/pages/halaman_toko.dart';
 import 'package:tubes_sparehub/pages/halaman_LoginAndRegister/login.dart';
 
@@ -17,10 +17,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      //home: const MyHomePage(title: 'SpareHub'),
-      // home: CheckoutApp(),
+      // Pilih halaman awal yang mau ditampilkan
       // home: ShopPage(),
-      home: LoginPage(),
+      // home: LoginPage(),
+      home: MyHomePage(title: 'SpareHub'),
     );
   }
 }
@@ -37,9 +37,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final PageController _pageController = PageController();
   int _selectedIndex = 1;
+
   final List<Widget> _pages = [
-    const Page2(), // Toko Saya (halaman Rziqi)
-    const HomePage(), // Home (grid produk)
+    ShopPage(), // Toko Saya
+    const HomePage(), // Home
     const EditProfil(), // Profil
   ];
 
@@ -49,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
     _pageController.animateToPage(
       index,
-      duration: const Duration(microseconds: 300),
+      duration: const Duration(milliseconds: 300),
       curve: Curves.easeOut,
     );
   }
@@ -65,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-        items: [
+        items: const [
           BottomNavigationBarItem(icon: Icon(Icons.store), label: "Toko Saya"),
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: "Saya"),
@@ -79,9 +80,11 @@ class Page2 extends StatelessWidget {
   const Page2({super.key});
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Scaffold(
-        body: Text("Halaman Rziqi disini", style: TextStyle(fontSize: 28)),
+        body: Center(
+          child: Text("Halaman Profil Saya", style: TextStyle(fontSize: 28)),
+        ),
       ),
     );
   }
