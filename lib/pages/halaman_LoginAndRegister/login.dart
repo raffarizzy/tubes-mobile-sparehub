0c1e3fce3d149d5c6081b16119fb6dbc411998db
Frizam <frizammaulana07@gmail.com>
2025-12-12 20:03:41 +0700

    nambah logo di login

--- lib/pages/halaman_LoginAndRegister/login.dart
+++ lib/pages/halaman_LoginAndRegister/login.dart
@@ -44,7 +44,7 @@ class _LoginPageState extends State<LoginPage> {
 
     // Cari user di UserData
     try {
-        UserModel? user = await _authService.signInWithEmailPassword(
+      UserModel? user = await _authService.signInWithEmailPassword(
         email: email,
         password: password,
       );
@@ -58,10 +58,8 @@ class _LoginPageState extends State<LoginPage> {
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
-              builder: (context) => MyHomePage(
-              title: 'SpareHub',
-              userData: user.toMap(),
-            ),
+            builder: (context) =>
+                MyHomePage(title: 'SpareHub', userData: user.toMap()),
           ),
         );
       } else {
@@ -115,14 +113,16 @@ class _LoginPageState extends State<LoginPage> {
                 Container(
                   width: 100,
                   height: 100,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF122C4F),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: const Icon(
-                    Icons.shopping_cart,
-                    size: 60,
-                    color: Colors.white,
+                  child: Image.asset(
+                    'assets/images/logo_baru_(with_border).png',
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      return const Icon(
+                        Icons.store,
+                        color: Color(0xFF122C4F),
+                        size: 24,
+                      );
+                    },
                   ),
                 ),
 
