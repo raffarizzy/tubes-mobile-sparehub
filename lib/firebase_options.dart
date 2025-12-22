a413e606639f2e94393d309c08cd91f0b7c27505
raffarizzy <raffarizkyf@gmail.com>
2025-12-22 18:21:42 +0700

    web support

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -51,6 +48,15 @@ class DefaultFirebaseOptions {
         );
     }
   }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyDORSHLt2M4QU_FNrgVSacUSYifP3dWiFE',
+    appId: '1:669277498622:web:569dbfda90ffbdb48dc9af',
+    messagingSenderId: '669277498622',
+    projectId: 'tubes-e0f9f',
+    authDomain: 'tubes-e0f9f.firebaseapp.com',
+    storageBucket: 'tubes-e0f9f.firebasestorage.app',
+  );
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyAhmcR8VSdHDl0AT0q8IIaBcdQLODeY51E',
